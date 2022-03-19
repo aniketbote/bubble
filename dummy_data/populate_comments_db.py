53754576cc7aa3c61f4028052c64205bc23367e5
Aniket Bote <ab9114@nyu.edu>
2022-03-19 00:16:40 -0400

    changed commentID-->comments_id

--- dummy_data/populate_comments_db.py
+++ dummy_data/populate_comments_db.py
@@ -23,7 +23,7 @@ comments_table = client.Table('comments-db')
 with comments_table.batch_writer() as batch:
     for i in range(comments_df.shape[0]):
         new_record = {}
-        new_record['commentID'] = commentIDs_list[i]
+        new_record['comments_id'] = commentIDs_list[i]
         new_record['comment'] = comment_list[i]
         new_record['timestamp'] = timestamp_list[i]
         batch.put_item(Item=new_record)
